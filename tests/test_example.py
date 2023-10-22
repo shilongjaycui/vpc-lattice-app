ed040646e81f1d668ca869cd6e28c6d86bf947f8
Jay Cui <shilongjaycui@gmail.com>
2023-10-22 01:16:49 -0600

    feat: add type annotations to `test_example.py`

--- tests/test_example.py
+++ tests/test_example.py
@@ -5,11 +5,11 @@ from aws_cdk.assertions import Template
 from vpc_lattice_app.main import MyStack
 
 @pytest.fixture(scope='module')
-def template():
-  app = App()
-  stack = MyStack(app, "my-stack-test")
-  template = Template.from_stack(stack)
+def template() -> Template:
+  app: App = App()
+  stack: MyStack = MyStack(app, "my-stack-test")
+  template: Template = Template.from_stack(stack)
   yield template
 
-def test_no_buckets_found(template):
+def test_no_buckets_found(template: Template):
   template.resource_count_is("AWS::S3::Bucket", 0)
