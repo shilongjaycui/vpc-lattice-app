eded7bb66877660ee5abf24f9b46c5da2b9173ac
Jay Cui <shilongjaycui@gmail.com>
2023-10-22 12:05:49 -0600

    feat: state machine stack unit tests

--- app.py
+++ app.py
@@ -1,15 +1,16 @@
 import os
 from aws_cdk import App, Environment
-from vpc_lattice_app.main import MyStack
+# from vpc_lattice_app.stack import MyStack
+
+
+app = App()
 
 # for development, use account/region from cdk cli
 dev_env = Environment(
   account=os.getenv('CDK_DEFAULT_ACCOUNT'),
   region=os.getenv('CDK_DEFAULT_REGION')
 )
-
-app = App()
-MyStack(app, "vpc-lattice-app-dev", env=dev_env)
+# MyStack(app, "vpc-lattice-app-dev", env=dev_env)
 # MyStack(app, "vpc-lattice-app-prod", env=prod_env)
 
 app.synth()
