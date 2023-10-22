5a5f382b07ce6c1cc62732fe7c13920511f26297
Jay Cui <shilongjaycui@gmail.com>
2023-10-22 00:38:43 -0600

    Added VS Code settings

--- vpc_lattice_app/main.py
+++ vpc_lattice_app/main.py
@@ -4,7 +4,6 @@ from constructs import Construct
 
 
 class MyStack(Stack):
-  def __init__(self, scope: Construct, construct_id: str, **kwargs) -> None:
-    super().__init__(scope, construct_id, **kwargs)
-
-    # The code that defines your stack goes here
+	def __init__(self, scope: Construct, construct_id: str, **kwargs) -> None:
+		super().__init__(scope, construct_id, **kwargs)
+		# The code that defines your stack goes here
